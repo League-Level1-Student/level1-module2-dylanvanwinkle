51905ce87b9a46ae0d3a4cb46bf63fafab319024
dylanvanwinkle <dylanvanwinkle@gmail.com>
2020-04-27 13:29:50 -0700

    SmufIP

--- src/_02_sea_creature/SeaCreature.java
+++ src/_02_sea_creature/SeaCreature.java
@@ -9,7 +9,7 @@ import java.applet.AudioClip;
 
 public class SeaCreature {
 	
-	// 1. In a Runner class, make a SeaCreature called “Spongebob”. Use the methods below to make him eat, and laugh.
+	// 1. In a Runner class, make a SeaCreature called Spongebob. Use the methods below to make him eat, and laugh.
 	// 2. Make Patrick and Squidward and print their name, have them eat, and make them laugh.
 
 
@@ -30,6 +30,7 @@ public class SeaCreature {
 	public void laugh() {
 		try {
 		    AudioClip sound = JApplet.newAudioClip(getClass().getResource(this.name + ".wav"));
+		    System.out.println("laugh");
 		    sound.play();
 			Thread.sleep(3400);
 		} catch (Exception ex) {
---
+++ src/_02_sea_creature/SeaCreatureRunner.java
@@ -0,0 +1,15 @@
+package _02_sea_creature;
+
+public class SeaCreatureRunner {
+public static void main(String[] args) {
+SeaCreature s = new SeaCreature("Spongebob");
+s.eat();
+s.laugh();
+SeaCreature s2 = new SeaCreature("Squidward");
+s2.eat();
+s2.laugh();
+SeaCreature s3 = new SeaCreature("Patrick");
+s3.eat();
+s3.laugh();
+}
+}
